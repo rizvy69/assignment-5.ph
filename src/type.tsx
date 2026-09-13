4ae3254ede87ca6a621fe767bb72e005c07f215f
Redowan Ahamed Rizvy <ridowanahamed69@gmail.com>
2026-09-13 12:21:41 +0600

    add typescript types

---
+++ src/type.tsx
@@ -0,0 +1,10 @@
+export default interface IData {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  icon: string;
+  rating: number;
+  difficulty: string;
+  badge: string;
+}
